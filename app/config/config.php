f38bb15b03cc4d586142a11251beee19157393eb
Cainta-PC-Gabriel <41713299+gabbbcapili@users.noreply.github.com>
2020-10-02 16:45:07 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://inventory.yuukihost.com/';
+$config['base_url'] = 'http://tadms.aquiware.com/';
 
 /*
 |--------------------------------------------------------------------------
