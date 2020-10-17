cb81cdab1b8ebfe043f4febf12cd2963db24810c
Cainta-PC-Gabriel <41713299+gabbbcapili@users.noreply.github.com>
2020-10-17 20:26:43 +0800

    configs
    configs

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://tadms.test/';
+$config['base_url'] = 'http://tadms.aquiware.com/';
 
 /*
 |--------------------------------------------------------------------------
